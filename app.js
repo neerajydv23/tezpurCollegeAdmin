0aacbb06e950c67057086f8206440ecd99ed80f2
Neeraj <yneeraj082r@gmail.com>
2024-03-21 16:36:49 +0530

    removed /app2

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express()
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public/app2')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(fileUpload({
   useTempFiles: true
